4a569d0cfadf08c12d3a791e1764055ee2999d6a
Steven Dao <stevedao@gmail.com>
2013-07-30 23:21:45 -0400

    Attributes; interactive creation
    Interactive creation works with a simple plane in bounding box.

--- TessendorfOcean/TessendorfOcean.cpp
+++ TessendorfOcean/TessendorfOcean.cpp
@@ -26,6 +26,7 @@ public:
 	virtual ~TessendorfOcean();
 
 	// Parameter block handled by parent
+	static IObjParam *ip; // Access to the interface
 
 	// From BaseObject
 	virtual CreateMouseCallBack* GetCreateMouseCallBack();
@@ -93,7 +94,16 @@ enum { tessendorfocean_params };
 
 //TODO: Add enums for various parameters
 enum { 
-	pb_spin,
+	pb_width,
+	pb_length,
+	pb_width_segs,
+	pb_length_segs,
+	pb_amplitude,
+	pb_min_wave_size,
+	pb_wind_speed,
+	pb_wind_direction,
+	pb_choppiness,
+	pb_seed
 };
 
 
@@ -104,15 +114,60 @@ static ParamBlockDesc2 tessendorfocean_param_blk ( tessendorfocean_params, _T("p
 	//rollout
 	IDD_PANEL, IDS_PARAMS, 0, 0, NULL,
 	// params
-	pb_spin, 			_T("spin"), 		TYPE_FLOAT, 	P_ANIMATABLE, 	IDS_SPIN, 
-		p_default, 		0.1f, 
-		p_range, 		0.0f,1000.0f, 
-		p_ui, 			TYPE_SPINNER,		EDITTYPE_FLOAT, IDC_EDIT,	IDC_SPIN, 0.01f, 
+	pb_width,			_T("width"),		TYPE_FLOAT,		P_ANIMATABLE,		IDS_WIDTH,
+		p_default,		100.0f,
+		p_range,		0.1f,10000.0f,
+		p_ui,			TYPE_SPINNER,		EDITTYPE_FLOAT,	IDC_WIDTH_EDIT,		IDC_WIDTH_SPIN,			0.1f,
+		p_end,
+	pb_length,			_T("length"),		TYPE_FLOAT,		P_ANIMATABLE,		IDS_LENGTH,
+		p_default,		100.0f,
+		p_range,		0.1f,10000.0f,
+		p_ui,			TYPE_SPINNER,		EDITTYPE_FLOAT,	IDC_LENGTH_EDIT,	IDC_LENGTH_SPIN,		0.1f,
+		p_end,
+	pb_width_segs,		_T("width_segs"),	TYPE_FLOAT,		0,					IDS_WIDTH_SEGS,
+		p_default,		100.0f,
+		p_range,		10.0f,1000.0f,
+		p_ui,			TYPE_SPINNER,		EDITTYPE_INT,	IDC_WIDTH_SEGS_EDIT,IDC_WIDTH_SEGS_SPIN,	1.0f,
+		p_end,
+	pb_length_segs,		_T("length_segs"),	TYPE_FLOAT,		0,					IDS_LENGTH_SEGS,
+		p_default,		100.0f,
+		p_range,		10.0f,1000.0f,
+		p_ui,			TYPE_SPINNER,		EDITTYPE_INT,	IDC_LENGTH_SEGS_EDIT,IDC_LENGTH_SEGS_SPIN,	1.0f,
+		p_end,
+	pb_amplitude, 		_T("amplitude"), 	TYPE_FLOAT, 	P_ANIMATABLE, 		IDS_AMPLITUDE, 
+		p_default, 		0.001f, 
+		p_range, 		0.0f,0.1f, 
+		p_ui, 			TYPE_SPINNER,		EDITTYPE_FLOAT, IDC_AMPLITUDE_EDIT,	IDC_AMPLITUDE_SPIN,		0.001f, 
+		p_end,
+	pb_min_wave_size,	_T("min_wave_size"),TYPE_FLOAT,		P_ANIMATABLE,		IDS_MIN_SIZE,
+		p_default,		1.0f,
+		p_range,		0.0f,50.0f,
+		p_ui,			TYPE_SPINNER,		EDITTYPE_FLOAT,	IDC_MIN_SIZE_EDIT,	IDC_MIN_SIZE_SPIN,		0.1f,
+		p_end,
+	pb_wind_speed,		_T("wind_speed"),	TYPE_FLOAT,		P_ANIMATABLE,		IDS_SPEED,
+		p_default,		2.0f,
+		p_range,		0.0f,20.0f,
+		p_ui,			TYPE_SPINNER,		EDITTYPE_FLOAT,	IDC_SPEED_EDIT,		IDC_SPEED_SPIN,			0.1f,
+		p_end,
+	pb_wind_direction,	_T("wind_dir"),		TYPE_ANGLE,		P_ANIMATABLE,		IDS_DIRECTION,
+		p_default,		0.0f,
+		p_ui,			TYPE_SPINNER,		EDITTYPE_FLOAT,	IDC_DIRECTION_EDIT,	IDC_DIRECTION_SPIN,		1.0f,
+		p_end,
+	pb_choppiness,		_T("choppiness"),	TYPE_FLOAT,		P_ANIMATABLE,		IDS_CHOPPINESS,
+		p_default,		0.5f,
+		p_range,		0.0f,2.0f,
+		p_ui,			TYPE_SPINNER,		EDITTYPE_FLOAT,	IDC_CHOPPINESS_EDIT,IDC_CHOPPINESS_SPIN,	0.1f,
+		p_end,
+	pb_seed,			_T("seed"),			TYPE_FLOAT,		0,		IDS_SEED,
+		p_default,		1.0f,
+		p_range,		1.0f,4000000000.0f,
+		p_ui,			TYPE_SPINNER,		EDITTYPE_FLOAT,	IDC_SEED_EDIT,		IDC_SEED_SPIN,			1.0f,
 		p_end,
 	p_end
 	);
 
 
+IObjParam *TessendorfOcean::ip = NULL;
 
 
 //--- TessendorfOcean -------------------------------------------------------
@@ -128,6 +183,8 @@ TessendorfOcean::~TessendorfOcean()
 
 void TessendorfOcean::BeginEditParams(IObjParam* ip, ULONG flags, Animatable* prev)
 {
+	this->ip = ip;
+
 	SimpleObject2::BeginEditParams(ip,flags,prev);
 	GetTessendorfOceanDesc()->BeginEditParams(ip, this, flags, prev);
 }
@@ -137,6 +194,8 @@ void TessendorfOcean::EndEditParams( IObjParam* ip, ULONG flags, Animatable* nex
 	//TODO: Save plugin parameter values into class variables, if they are not hosted in ParamBlocks. 
 	SimpleObject2::EndEditParams(ip,flags,next);
 	GetTessendorfOceanDesc()->EndEditParams(ip, this, flags, next);
+
+	this->ip = NULL;
 }
 
 //From Object
@@ -155,9 +214,10 @@ void TessendorfOcean::SetGenUVW(BOOL sw)
 
 //Class for interactive creation of the object using the mouse
 class TessendorfOceanCreateCallBack : public CreateMouseCallBack {
-	IPoint2 sp0;              //First point in screen coordinates
-	TessendorfOcean* ob; //Pointer to the object 
-	Point3 p0;                //First point in world coordinates
+	IPoint2 sp0;            //First point in screen coordinates
+	TessendorfOcean* ob;	//Pointer to the object 
+	Point3 p0;				//First point in world coordinates
+	Point3 p1;				//Second point in world coordinates
 public:	
 	int proc( ViewExp *vpt,int msg, int point, int flags, IPoint2 m, Matrix3& mat);
 	void SetObj(TessendorfOcean *obj) {ob = obj;}
@@ -175,13 +235,35 @@ int TessendorfOceanCreateCallBack::proc(ViewExp *vpt,int msg, int point, int /*f
 
 	if (msg == MOUSE_POINT || msg == MOUSE_MOVE) {
 		switch(point) {
-		case 0: // only happens with MOUSE_POINT msg
-			ob->suspendSnap = TRUE;
-			sp0 = m;
-			p0 = vpt->SnapPoint(m,m,NULL,SNAP_IN_PLANE);
-			mat.SetTrans(p0);
-			break;
-		//TODO: Add for the rest of points
+			case 0: // only happens with MOUSE_POINT msg
+				ob->suspendSnap = TRUE;
+				sp0 = m;
+				p0 = vpt->SnapPoint(m,m,NULL,SNAP_IN_PLANE);
+				mat.SetTrans(p0);
+
+				//Set a default overall size in the parameter block
+				ob->pblock2->SetValue(pb_width, ob->ip->GetTime(), 0.0f);
+				ob->pblock2->SetValue(pb_length, ob->ip->GetTime(), 0.0f);
+				break;
+			case 1:
+			{
+				ob->suspendSnap = TRUE; 
+				p1 = vpt->SnapPoint(m,m,NULL,SNAP_IN_PLANE);
+				Point3 diff = p1 - p0;
+				mat.SetTrans(p0 + diff / 2.0f);
+				
+				//Set the overall size in parameter block
+				ob->pblock2->SetValue(pb_width, ob->ip->GetTime(), abs(diff.x));
+				ob->pblock2->SetValue(pb_length, ob->ip->GetTime(), abs(diff.y));
+
+				//Invalidate and display the mesh in the viewport
+				tessendorfocean_param_blk.InvalidateUI();
+				break;
+			}
+			case 2:
+			{
+				return CREATE_STOP;
+			}
 		}
 	} else {
 		if (msg == MOUSE_ABORT) return CREATE_ABORT;
@@ -200,7 +282,7 @@ CreateMouseCallBack* TessendorfOcean::GetCreateMouseCallBack()
 }
 
 //From SimpleObject
-void TessendorfOcean::BuildMesh(TimeValue /*t*/)
+void TessendorfOcean::BuildMesh(TimeValue t)
 {
 	//TODO: Implement the code to build the mesh representation of the object
 	//      using its parameter settings at the time passed. The plug-in should 
@@ -208,6 +290,25 @@ void TessendorfOcean::BuildMesh(TimeValue /*t*/)
 	//      SimpleObject ivalid member should be updated. This can be done while
 	//      retrieving values from pblock2.
 	ivalid = FOREVER;
+
+	float length, width;
+	pblock2->GetValue(pb_length, t, length, ivalid );
+	pblock2->GetValue(pb_width, t, width, ivalid );
+
+	mesh.setNumVerts(4);
+	mesh.setNumFaces(2);
+	mesh.setVert(0, -width / 2.0f, -length / 2.0f, 0.0f);
+	mesh.setVert(1, -width / 2.0f, length / 2.0f, 0.0f);
+	mesh.setVert(2, width / 2.0f, length / 2.0f, 0.0f);
+	mesh.setVert(3, width / 2.0f, -length / 2.0f, 0.0f);
+	mesh.faces[0].setVerts(0, 1, 2);
+	mesh.faces[0].setEdgeVisFlags(1,1,0);
+	mesh.faces[0].setSmGroup(0);
+	mesh.faces[1].setVerts(2, 3, 0);
+	mesh.faces[1].setEdgeVisFlags(1,1,0);
+	mesh.faces[1].setSmGroup(0);
+	
+	mesh.InvalidateGeomCache();
 }
 
 void TessendorfOcean::InvalidateUI() 
--- TessendorfOcean/resource.h
+++ TessendorfOcean/resource.h
@@ -1,36 +1,57 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Developer Studio generated include file.
+// Microsoft Visual C++ generated include file.
 // Used by TessendorfOcean.rc
 //
-
-
 #define IDS_LIBDESCRIPTION              1
 #define IDS_CATEGORY                    2
 #define IDS_CLASS_NAME                  3
 #define IDS_PARAMS                      4
 #define IDS_SPIN                        5
-
-
-
-
-
-
-
+#define IDS_AMPLITUDE                   5
+#define IDS_WIDTH                       6
+#define IDS_LENGTH                      7
+#define IDS_WIDTH_SEGS                  8
+#define IDS_LENGTH_SEGS                 9
+#define IDS_MIN_SIZE                    10
+#define IDS_SPEED                       11
+#define IDS_DIRECTION                   12
+#define IDS_CHOPPINESS                  13
+#define IDS_SEED                        14
 #define IDD_PANEL                       101
-
 #define IDC_CLOSEBUTTON                 1000
 #define IDC_DOSTUFF                     1000
+#define IDC_AMPLITUDE_EDIT              1000
+#define IDC_WIDTH_EDIT                  1001
+#define IDC_MIN_SIZE_EDIT               1002
+#define IDC_LENGTH_EDIT                 1003
+#define IDC_WIDTH_SEGS_EDIT             1004
+#define IDC_LENGTH_SEGS_EDIT            1005
+#define IDC_SPEED_EDIT                  1006
+#define IDC_DIRECTION_EDIT              1007
+#define IDC_CHOPPINESS_EDIT             1008
+#define IDC_SEED_EDIT                   1009
+#define IDC_TIME_EDIT                   1010
 #define IDC_COLOR                       1456
 #define IDC_EDIT                        1490
 #define IDC_SPIN                        1496
+#define IDC_AMPLITUDE_SPIN              1496
+#define IDC_WIDTH_SPIN                  1497
+#define IDC_LENGTH_SPIN                 1498
+#define IDC_WIDTH_SEGS_SPIN             1499
+#define IDC_LENGTH_SEGS_SPIN            1500
+#define IDC_MIN_SIZE_SPIN               1501
+#define IDC_SPEED_SPIN                  1502
+#define IDC_DIRECTION_SPIN              1503
+#define IDC_CHOPPINESS_SPIN             1504
+#define IDC_SEED_SPIN                   1505
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        101
+#define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_CONTROL_VALUE         1003
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
