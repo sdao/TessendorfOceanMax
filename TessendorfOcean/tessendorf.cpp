232d674461d35e017f254b536da4c1a21a236e0a
Steven Dao <stevedao@gmail.com>
2013-07-31 21:52:13 -0400

    Commit simulation and FFT code

---
+++ TessendorfOcean/tessendorf.cpp
@@ -0,0 +1,141 @@
+#define _USE_MATH_DEFINES
+#include <cmath>
+
+#include "tessendorf.h"
+#include "helpers.h"
+#include "kiss_fft130/kissfft.hh"
+
+tessendorf::tessendorf(float amplitude, float speed, Point3 direction, float choppiness, float time, float phaseDuration, int resX, int resY, float scaleX, float scaleY, float waveSizeLimit, int rngSeed)
+{
+    // Parameters.
+    A = amplitude;
+    V = speed;
+    w_hat = direction.FNormalize();
+    t = time;
+    lambda = choppiness;
+    M = resX;
+    N = resY;
+    Lx = scaleX;
+    Ly = scaleY;
+    l = waveSizeLimit;
+    T = phaseDuration;
+    omega_0 = 2. * M_PI / T;
+    seed = rngSeed;
+    vertices = new Point3[M*N];
+
+    // Precalculate known constants.
+    P_h__L = pow(V, 2) / GRAVITY;
+    P_h__l_2 = pow(l, 2);
+}
+
+tessendorf::~tessendorf()
+{
+    delete [] vertices;
+}
+
+float tessendorf::omega(Point3 k)
+{
+    return floor(sqrt(GRAVITY * k.FLength()) / omega_0) * omega_0;
+}
+
+float tessendorf::P_h(Point3 k)
+{
+    float k_length = k.FLength();
+
+    if (k_length < DBL_EPSILON) {
+        return 0.; // Avoid divison by zero error.
+    }
+
+    Point3 k_hat = k.FNormalize();
+
+    float nomin = exp(-1. / pow(k_length * P_h__L, 2));
+    float denom = pow(k_length, 4);
+    float scale = exp(-pow(k_length, 2) * P_h__l_2);
+
+    return A * nomin / denom * pow(DotProd(k_hat, w_hat), 2) * scale;
+}
+
+complex tessendorf::h_tilde_0(Point3 k)
+{
+    complex xi = random_gaussian_complex();
+
+    return xi * (float)sqrt(P_h(k) / 2.);
+}
+
+complex tessendorf::h_tilde(Point3 k)
+{
+    complex h_tilde_0_k = h_tilde_0(k);
+    complex h_tilde_0_k_star = h_tilde_0(-k);
+
+    float omega_k_t = omega(k) * t;
+
+    float cos_omega_k_t = cos(omega_k_t);
+    float sin_omega_k_t = sin(omega_k_t);
+
+    complex c0(cos_omega_k_t, sin_omega_k_t);
+    complex c1(cos_omega_k_t, -sin_omega_k_t);
+
+    return h_tilde_0_k * c0 + h_tilde_0_k_star * c1;
+}
+
+Point3* tessendorf::simulate()
+{
+    srand(seed);
+
+    complex* h_tildes_in = new complex[M*N];
+    complex* disp_x_in = new complex[M*N];
+    complex* disp_y_in = new complex[M*N];
+
+    complex* h_tildes_out = new complex[M*N];
+    complex* disp_x_out = new complex[M*N];
+    complex* disp_y_out = new complex[M*N];
+
+    for (int m = 0; m < M; m++) {
+        for (int n = 0; n < N; n++) {
+            int index = m * N + n;
+
+            int m_ = m - M / 2;  // m coord offsetted.
+            int n_ = n - N / 2; // n coord offsetted.
+
+            Point3 k(2. * M_PI * n_ / Lx, 2. * M_PI * m_ / Ly, 0.);
+
+            complex h_tilde_k = h_tilde(k);
+            h_tildes_in[index] = h_tilde_k;
+
+            Point3 k_hat = k.FNormalize();
+            disp_x_in[index] = complex(0., -k_hat.x) * h_tilde_k; // Displacement by equation (29).
+            disp_y_in[index] = complex(0., -k_hat.y) * h_tilde_k;
+        }
+    }
+
+    kissfft<float> fft(M*N, false);
+    fft.transform(h_tildes_in, h_tildes_out);
+    fft.transform(disp_x_in, disp_x_out);
+    fft.transform(disp_y_in, disp_y_out);
+
+    float signs[2] = { -1., 1. };
+
+    for (int m = 0; m < M; m++) {
+        for (int n = 0; n < N; n++) {
+            int index = m * N + n;
+            int sign = signs[(m + n) & 1]; // Sign-flip all of the odd coefficients.
+
+            int m_ = m - M / 2;  // m coord offsetted.
+            int n_ = n - N / 2;  // n coord offsetted.
+
+            Point3 x(n_ * Lx / N + real(disp_x_out[index]) * lambda * sign,
+                m_ * Ly / M + real(disp_y_out[index]) * lambda * sign,
+                real(h_tildes_out[index]) * sign);
+            vertices[index] = x;
+        }
+    }
+
+    delete [] h_tildes_in;
+    delete [] disp_x_in;
+    delete [] disp_y_in;
+    delete [] h_tildes_out;
+    delete [] disp_x_out;
+    delete [] disp_y_out;
+
+    return vertices;
+}
